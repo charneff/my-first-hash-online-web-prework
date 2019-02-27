a3af4172fd7e95f901fed192bb7a3009e5f63011
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-27 20:48:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,13 +13,12 @@ def shipping_manifest
 end
 
 def retrieval
+  shipping_manifest["oil paintings"]
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  shipping manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
